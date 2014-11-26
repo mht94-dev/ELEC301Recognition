fb1efacc6a35e5f9693301375d16d9505afc9a17
mht94 <mht_94@hotmail.com>
2014-11-26 11:39:07 -0600

    Possibly working example

--- Sample.m
+++ Sample.m
@@ -1,19 +1,15 @@
+clear 
+clc
+
 Template = imread('Template.jpg');   % Reads the images
 Image = imread('Image.jpg');
 
 % imshow(Image)         % Displays the images
 % figure
 % imshow(Template)
-
-dimT = size(Template);
-dimI = size(Image);
-
-c = 0;
-for i=1:dimI(1)-dimT(1) 
-    for j=1:dimI(2)-dimT(2)
-        dotproduct = Template.*Image(i:dimT(1)+i-1,j:dimT(2)+j-1,:);
-    end
-    
-end
-
-plot(dotproduct)
+Image = rgb2gray(Image);
+Template = rgb2gray(Template);
+Image = double(Image);
+Template = double(Template);
+figure; imagesc(conv2(Image,fliplr(flipud(Template))))
+figure; imagesc(conv2(Image,fliplr(flipud(Template))))
