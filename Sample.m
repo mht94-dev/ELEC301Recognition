300f3732972d27f764809e8d59062a9a73e8017a
mht94 <mht_94@hotmail.com>
2014-11-26 20:08:56 -0600

    Latest working example

--- Sample.m
+++ Sample.m
@@ -1,15 +1,31 @@
 clear 
 clc
 
-Template = imread('Template.jpg');   % Reads the images
-Image = imread('Image.jpg');
+%Template = imread('Template.jpg');   % Reads the images
+%Image = imread('Image.jpg');
 
+Triangle = imread('Triangle.jpg');
+Square = imread('Square.jpg');
+Circle = imread('Circle.jpg');
+TestImage = imread('TestImage.jpg');
+
+Triangle = double(rgb2gray(Triangle));
+Square = double(rgb2gray(Square));
+Circle = double(rgb2gray(Circle));
+TestImage = double(rgb2gray(TestImage));
+figure;imagesc(conv2(TestImage,fliplr(flipud(Triangle))));
+title('Triangle');
+figure;imagesc(conv2(TestImage,fliplr(flipud(Circle))));
+title('Circle');
+figure;imagesc(conv2(TestImage,fliplr(flipud(Square))));
+title('Square');
 % imshow(Image)         % Displays the images
 % figure
 % imshow(Template)
-Image = rgb2gray(Image);
-Template = rgb2gray(Template);
-Image = double(Image);
-Template = double(Template);
-figure; imagesc(conv2(Image,fliplr(flipud(Template))))
-figure; imagesc(conv2(Image,fliplr(flipud(Template))))
+
+% Image = rgb2gray(Image);
+% Template = rgb2gray(Template);
+% Image = double(Image);
+% Template = double(Template);
+% figure; imagesc(conv2(Image,fliplr(flipud(Template))))
+%figure; imagesc(conv2(Image,fliplr(flipud(Template))))
